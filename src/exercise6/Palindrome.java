e6ba93cda99c414965a8ab93c4782fb41d3a4e49
SkyDoge69 <luc2002penta@gmail.com>
2021-11-17 11:49:19 +0200

    Again

--- src/exercise6/Palindrome.java
+++ src/exercise6/Palindrome.java
@@ -1,9 +1,26 @@
 package exercise6;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Palindrome {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int sum = 0;
+        int temp = n;
+        boolean is_palindrome = true;
+//        ArrayList<Integer> array = new ArrayList<Integer>();
+        while (n!=0) {
+            int a = n % 10;
+            sum = (sum*10) + a;
+            n = n / 10;
+        }
+
+        if (temp != sum) {
+            is_palindrome = false;
+        }
+
+        System.out.println("Palindrome: " + is_palindrome);
     }
 }
